ee47b18735f73c427570553814f6c439b45793ec
macbookpro <macbookpro@macbookpros-MacBook-Pro.local>
2016-09-13 05:08:42 -0700

    second commit

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ var app = express();
 
 var compiler = webpack(config);
 
-mongoose.connect(process.env.MONGODB);
+mongoose.connect('mongodb://localhost:27017/react-todo');
 mongoose.connection.on('error', function() {
   console.log('MongoDB Connection Error. Please make sure that MongoDB is running.');
   process.exit(1);
